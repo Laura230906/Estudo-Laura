6f72734fde36873dc0b6d390c99472c7766ffb1a
Laura230906 <0000109331493XSP@al.educacao.sp.gov.br>
2024-11-28 11:30:12 -0300

    Add files via upload

---
+++ Laura_Estudo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'Quanto é 10x20?',
+    '200'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Historia',
+    'De onde é a invenção do chuveiro eletrico?',
+    'Brasil'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
